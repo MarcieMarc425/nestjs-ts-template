0a2bb57af47669be3c5955e499ea1939d57c7a81
Marc Tse <marctse@gmail.com>
2022-02-27 20:46:29 +0800

    build(versionrc): update standard-version config

--- .versionrc.js
+++ .versionrc.js
@@ -1,4 +1,50 @@
 module.exports = {
   issueUrlFormat: '{{prefix}}{{id}}', // JIRA issue URL format
   issuePrefixes: ['PREFIX'], // JIRA issue prefixes
+  types: [
+    {
+      type: 'feat',
+      section: 'Features 🎯',
+    },
+    {
+      type: 'fix',
+      section: 'Bug Fixes 🐛',
+    },
+    {
+      type: 'chore',
+      section: 'Chores 🧹',
+    },
+    {
+      type: 'docs',
+      section: 'Documentation 📖',
+    },
+    {
+      type: 'style',
+      section: 'Styles 💅',
+    },
+    {
+      type: 'refactor',
+      section: 'Refactor 🔄',
+    },
+    {
+      type: 'perf',
+      section: 'Performance 🏎',
+    },
+    {
+      type: 'test',
+      section: 'Tests 🧪',
+    },
+    {
+      type: 'build',
+      section: 'Build System 🛠',
+    },
+    {
+      type: 'ci',
+      section: 'Continuous Integration 🚀',
+    },
+    {
+      type: 'revert',
+      section: 'Reverts ⏪',
+    },
+  ],
 };
